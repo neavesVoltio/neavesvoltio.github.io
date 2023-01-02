9d7fee82b03ec8c68ae6fe25b8f98b1f54e70746
Neaves Voltio <neaves@voltio.us>
2023-01-02 01:15:39 -0600

    carrito de compras

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ import { loginCheck } from './app/loginCheck.js'
 
 
 import './app/logout.js'
+<<<<<<< Updated upstream
 export let webDomain = "https://neavesvoltio.github.io"
+=======
+//import './app/signinForm.js'
+//import './app/googleLogin.js'
+export let webDomain = "http://localhost:5500"
+>>>>>>> Stashed changes
 // http://localhost:5500
 // https://neavesvoltio.github.io
   
